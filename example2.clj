84aefdbbfb936abe32131f6b027814f6ca394190
gimhema <49871739+gimhema@users.noreply.github.com>
2023-07-23 20:29:38 +0900

    Commit 20230723
    if statement, loop test

--- example2.clj
+++ example2.clj
@@ -1,58 +1,114 @@
-(ns example2)
+;; (ns example2)
 
-(defn SomeFunction []
+;; (defn SomeFunction []
   
-  (def someVal 1)
+;;   (def someVal 1)
 
-  (def someVal2 2.3)
+;;   (def someVal2 2.3)
 
-  (def someVal3 "Calc Result : ") 
+;;   (def someVal3 "Calc Result : ") 
   
-  (def someVal4 
-    (+ someVal someVal2)
-    ) 
+;;   (def someVal4 
+;;     (+ someVal someVal2)
+;;     ) 
   
-  (println someVal3 someVal4)
+;;   (println someVal3 someVal4)
 
-  )
+;;   )
 
-(SomeFunction)
+;; (SomeFunction)
 
-(defn LoopExample []
+;; (defn LoopExample []
   
-  (def x (atom 1))
-  ( while ( < @x 5) 
-   (do 
-     (println "X : " @x)
-     (swap! x inc)
-     ) 
+;;   (def x (atom 1))
+;;   ( while ( < @x 5) 
+;;    (do 
+;;      (println "X : " @x)
+;;      (swap! x inc)
+;;      ) 
+;;    ) 
+;;   )
+
+;; (LoopExample)
+
+
+;; (defn DoSeqTest []
+
+;; (println "Do Seq Test")  
+;;   (doseq [n [0 1 2 3 4 5]] 
+;;     (println n)
+;;     )
+  
+;;   (println "Do Seq Test Use Variable")
+;;   (def n2 [0 1 2 3 4 5 6 7 8 9 10])
+
+;;   (doseq [i n2]
+;;     (println i)
+;;     ) 
+;;   )
+;; (DoSeqTest)
+
+;; (defn DoTimeTest [] 
+;;   (dotimes [n 10]
+;;     (println "Do Time Call : " n " Counted")
+;;     ) 
+;;   )
+;; (DoTimeTest)
+
+
+;; (defn LoopTest []
+  
+;;   (loop [x 10]
+;;     (when (> x 1)
+;;       (println x)
+;;       (recur (- x 2)) 
+;;       ) 
+;;     ) 
+;;   )
+;; (LoopTest)
+
+(defn IfConditionCheck [input]
+     (println "If Condition Checking Function .")
+  (
+   if (= input "Hi")
+   (println "Valid Check : " input)
+   (println "Invalid Check : " input)
    ) 
   )
 
-(LoopExample)
+;; (IfConditionCheck "Hi")
+;; (IfConditionCheck "Hello")
 
-
-(defn DoSeqTest []
-
-(println "Do Seq Test")  
-  (doseq [n [0 1 2 3 4 5]] 
-    (println n)
+(defn IfDoConditionCheck [x y]
+  (println "If Do Condition Checking . .")
+  (
+   if (= x y)
+    (do (println "X : " x " Y : " y)
+        (println "X & Y Same"))
+    (do (println "X : " x " Y : " y)
+        (println "X & Y Diffrent"))
     )
-  
-  (println "Do Seq Test Use Variable")
-  (def n2 [0 1 2 3 4 5 6 7 8 9 10])
+)
 
-  (doseq [i n2]
-    (println i)
-    ) 
-  )
-(DoSeqTest)
+;; (IfDoConditionCheck 1 1)
+;; (IfDoConditionCheck 1 7)
 
-(defn DoTimeTest [] 
-  (dotimes [n 10]
-    (println "Do Time Call : " n " Counted")
-    ) 
+(defn NestedIfConditionCheck [x y]
+  (println "If Nested Condition Checking . .")
+  (
+   if( and (= x "X") (= y "Y"))
+   (do (println "X : " x " Y : " y)
+       (println "Check True Nested If Condition"))
+   (do (println "X : " x " Y : " y)
+       (println "Check False Nested If Condition"))
+   ) 
   )
-(DoTimeTest)
+
+;; (NestedIfConditionCheck "X" "Y")
+;; (NestedIfConditionCheck "X" "B")
+;; (NestedIfConditionCheck "A" "Y")
+;; (NestedIfConditionCheck "A" "B")
+
+
 
 
