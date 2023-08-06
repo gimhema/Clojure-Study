41f094fbfca72dc9c59e1eef6fd0aeeb05081eaa
gimhema <49871739+gimhema@users.noreply.github.com>
2023-08-06 12:28:30 +0900

    Update ps1.clj
    .

--- PS/ps1.clj
+++ PS/ps1.clj
@@ -1,8 +1,14 @@
 (defn sum [x] (reduce + x))
 
-(println (sum [1 20 300]) ) 
+; (println (sum [1 20 300]) ) 
 
 (defn reduceExample [x] (reduce - x))
 
-(println (reduceExample [10 5 3]) )
+; (println (reduceExample [10 5 3]) )
+
+(defn reduceExample2 [x] 
+  (reduce * 3 x ) 
+  )
+
+(println (reduceExample2 [1 2 3]) )
 
