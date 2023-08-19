cb895816cb48ee9fd876fe942018e942b9289626
gimhema <49871739+gimhema@users.noreply.github.com>
2023-08-19 11:28:06 +0900

    Update ps1.clj
    .

--- PS/ps1.clj
+++ PS/ps1.clj
@@ -10,5 +10,18 @@
   (reduce * 3 x ) 
   )
 
-(println (reduceExample2 [1 2 3]) )
+; (println (reduceExample2 [1 2 3]) )
+
+(defn twoArrayExample [a b]
+  (doseq [elem1 a
+          elem2 b] 
+    (println "A : " elem1 "B : " elem2)
+    )
+  )
+
+
+(def vec1 (vector-of :int 1 2 3))
+(def vec2 (vector-of :int 4 5 6))
+(println (twoArrayExample vec1 vec2)) 
+
 
